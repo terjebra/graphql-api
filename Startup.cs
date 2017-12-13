4c8f4688a7d7175eae9beeba6217cf04d8dd5fc1
Terje Brådland <terje@localhost.localdomain>
2017-12-13 19:53:46 +0100

    Added cors

--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,17 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+             services.AddCors(options =>
+    {
+        options.AddPolicy("CorsPolicy",
+            builder => builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials() );
+    });
+    
+    
+    services.AddMvc(); 
             services.AddSingleton<IDocumentExecuter, DocumentExecuter>();
             services.AddSingleton<IDocumentWriter, DocumentWriter>();
 
@@ -53,6 +64,8 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env,ILoggerFactory loggerFactory)
         {
+             app.UseCors("CorsPolicy");
+             
             loggerFactory.AddConsole();
 
             app.UseMiddleware<GraphQLMiddleware>(new GraphQLSettings
