185680668a6ac7b4a71b192f073f52569f0dd9e7
Terje Brådland <terje@localhost.localdomain>
2018-03-27 20:51:09 +0200

    Updated graphql-libs. Removed code that is now part of the server-lib

--- Startup.cs
+++ Startup.cs
@@ -12,7 +12,6 @@ using Microsoft.Extensions.Options;
 using GraphQL.Http;
 using Microsoft.AspNetCore.Http;
 using GraphQL;
-using API.Middleware;
 using API.Command.Commands;
 using API.Command;
 using API.GraphQL;
@@ -21,72 +20,68 @@ using API.Query;
 using API.Infrastructure;
 using GraphQL.Server.Transports.AspNetCore;
 using GraphQL.Server.Transports.WebSockets;
-using GraphQL.Server.Transports.WebSockets.Events;
 
 namespace API
 {
-    public class Startup
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-           
-            services.AddSingleton<IDocumentExecuter, DocumentExecuter>();
-            services.AddSingleton<IDocumentWriter, DocumentWriter>();
-            services.AddSingleton<IRepository<Domain.TemperatureReading>, InMemoryTemperatureRepository>();
-            services.AddSingleton<IRepository<Domain.Room>, InMemoryRoomRepository>();
-            services.AddSingleton<ITemperatureCommandService, TemperatureCommandService>();
-            services.AddSingleton<IRoomCommandService,RoomCommandService>();
-            services.AddSingleton<ITemperatureQueryService, TemperatureQueryService>();
-            services.AddSingleton<IRoomQueryService, RoomQueryService>();
-            services.AddSingleton<API.GraphQL.Query>();
-            services.AddSingleton<Mutation>();
-            services.AddSingleton<Subscription>();
-            services.AddSingleton<TemperatureReading>();
-            services.AddSingleton<Room>();
-            services.AddSingleton<GraphQL.Types.RegisterTemperatureReading>();
-            services.AddSingleton<GraphQL.Types.RegisterRoom>();
-            
-            services.AddSingleton<APISchema>(
-                s => new APISchema(new FuncDependencyResolver(type => (GraphType) s.GetService(type))));
 
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            services.AddSingleton<IEventAggregator, SimpleEventAggregator>();
-            services.AddSingleton<API.Event.IEventHandler,API.Event.EventHandler>();
-            services.AddGraphQLHttp();
-            services.AddGraphQLWebSocket<APISchema>();
-            
-            services.AddCors(options =>
-            {
-                options.AddPolicy("CorsPolicy",
-                    builder => builder.AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials() 
-                );
-            });
-            
-            services.AddMvc(); 
-        }
-    
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env,ILoggerFactory loggerFactory)
-        {
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            
-            app.UseCors("CorsPolicy");
-             
-            loggerFactory.AddConsole();
+      services.AddSingleton<IRepository<Domain.TemperatureReading>, InMemoryTemperatureRepository>();
+      services.AddSingleton<IRepository<Domain.Room>, InMemoryRoomRepository>();
+      services.AddSingleton<ITemperatureCommandService, TemperatureCommandService>();
+      services.AddSingleton<IRoomCommandService, RoomCommandService>();
+      services.AddSingleton<ITemperatureQueryService, TemperatureQueryService>();
+      services.AddSingleton<IRoomQueryService, RoomQueryService>();
+      services.AddSingleton<API.GraphQL.Query>();
+      services.AddSingleton<Mutation>();
+      services.AddSingleton<Subscription>();
+      services.AddSingleton<TemperatureReading>();
+      services.AddSingleton<Room>();
+      services.AddSingleton<GraphQL.Types.RegisterTemperatureReading>();
+      services.AddSingleton<GraphQL.Types.RegisterRoom>();
 
-            app.UseWebSockets();
-            app.UseGraphQLWebSocket<APISchema>(new GraphQLWebSocketsOptions( ){Path ="/api/graphql"});
-            app.UseGraphQLHttp<APISchema>(
-                new GraphQLHttpOptions{
-                    Path ="/api/graphql"
-                }
-            );
+      services.AddSingleton<APISchema>(
+          s => new APISchema(new FuncDependencyResolver(type => (GraphType)s.GetService(type))));
 
-            app.UseMvc();
-        }
+      services.AddSingleton<API.Event.IEventHandler, API.Event.EventHandler>();
+      services.AddGraphQLHttp();
+      services.AddGraphQLWebSocket<APISchema>();
+
+      services.AddCors(options =>
+      {
+        options.AddPolicy("CorsPolicy",
+                  builder => builder.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader()
+                  .AllowCredentials()
+              );
+      });
+
+      services.AddMvc();
     }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
+
+      app.UseCors("CorsPolicy");
+
+      loggerFactory.AddConsole();
+
+      app.UseWebSockets();
+      app.UseGraphQLWebSocket<APISchema>(new GraphQLWebSocketsOptions() { Path = "/api/graphql" });
+      app.UseGraphQLHttp<APISchema>(
+          new GraphQLHttpOptions
+          {
+            Path = "/api/graphql"
+          }
+      );
+
+      app.UseMvc();
+    }
+  }
 }
